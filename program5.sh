8cb190ce3d87122636ea67ed2afffccdc358c95d
Sheryar-Ahmad <sheryarahmad311@gmail.com>
2026-04-22 11:45:48 +0500

    Show week days

---
+++ program5.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Enter the choice"
+read choice
+
+case $choice in
+ 1) 
+  echo "Monday" ;;
+ 2)
+  echo "Tuesday" ;;
+ 3) 
+  echo "Wednesday" ;;
+ 4)
+  echo "Thursday" ;;
+ 5) 
+  echo "Friday" ;;
+ 6)
+  echo "Saturday" ;;
+ 7) 
+  echo "Sunday" ;;
+ *)
+  echo "You entered wrong day number";;
+esac
+
