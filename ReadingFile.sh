5ad6ee9d06b55cba73f0e5d2e1328165adbb4641
Sheryar-Ahmad <sheryarahmad311@gmail.com>
2026-04-29 11:42:07 +0500

    Show data from the file you want to see

--- ReadingFile.sh
+++ ReadingFile.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/bash
 
-file=/home/sheheryar/oslab/KillProcess.sh
+echo "Enter your user name"
+read username
+echo "Enter your Directory name from where you want to read the file"
+read dir
+echo "Enter your file name you want to Display"
+read filename
+file=/home/$username/$dir/$filename
 
 while read -r line;
 do 
